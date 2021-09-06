ccb94973f86cfdd2d8525bbc0ad0f874e6f37a5a
Kaiichiro Ota <kigh.ota@gmail.com>
2021-09-07 03:27:01 +0900

    implement display using JavaFX

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
     kotlin("jvm") version "1.5.10"
+    id("org.openjfx.javafxplugin") version "0.0.8"
+}
+
+javafx {
+    modules("javafx.controls", "javafx.graphics")
 }
 
 group = "org.example"
